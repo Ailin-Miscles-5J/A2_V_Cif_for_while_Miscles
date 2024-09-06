f0cd74cb3057b4df21820352b0fb3ddb76858a94
Ailin-Miscles-5J <a.22308051281264@cbtis128.edu.mx>
2024-09-06 09:37:28 -0600

    Add files via upload
    Agregue dos archivos

---
+++ miscadenas.py
@@ -0,0 +1,22 @@
+'''Ailin Ruvlcaba 
+Trabajando con Cadenas'''
+
+nombre="Ailin"
+print(nombre[0])
+print(nombre)
+
+susnovias=["E","l","i"]
+for chicas in susnovias:
+    print(chicas)
+
+print("otro ejemplo")
+b = "Ailin Ruvalcaba"
+print(b[2:5])
+print(" a mayusculas")
+a = "ailin ruv"
+print(a.upper())
+print("Concatenar")
+a = "Maria"
+b = "Perez"
+c = a + " "+b
+print(c)
---
+++ mivariables.py
@@ -0,0 +1,18 @@
+import math
+edad=16 #16 se guarda en edad
+
+nombre="Ruvalcaba Ailin"
+print("tu edad es:" ,edad)
+print("te llamas:" ,nombre)
+print("--Tipo de Variabes--")
+
+x = str(7)    # funcion cadena
+y = int(3.1415)    # entero
+z = float(7.0)  # decimal
+print(x)
+print(y)
+print(z)
+print ("--Funciones Matmaticas--")
+x = math.sqrt(64)
+
+print(x)
